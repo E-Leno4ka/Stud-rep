a125235411a9753900d8037d5f5ea7241134d211
E-Leno4ka <egrechman@gmail.com>
2024-11-29 17:03:24 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,23 +1,31 @@
 mv=('+', '-', '*', '/', '**')
+
 try:
     a=int(input('First num: '))
-    m=str(input('Math operation (+, -, *, /): '))
-    b=int(input('Second num: '))
-except ValueError:
+    if not isinstance(a,int):
+        print('Введены некорректные данные. Попробуйте ещё раз.')
+    else:
+        m=str(input('Math operation (+, -, *, /): '))
+        if m not in mv:
+            print(f"Я не умею выполнять арифметическое действие '{m}' ")
+        else:
+            b=int(input('Second num: '))
+            if not isinstance(b,int):
+                print('Введены некорректные данные. Попробуйте ещё раз.')
+            else:
+                if m == '/' and b == 0:
+                    print('Деление на ноль недопустимо. Попробуйте ещё раз.')
+                else:
+                    if m == '+':
+                        res = a + b
+                    elif m == '-':
+                        res = a - b
+                    elif m == '*':
+                        res = a * b
+                    elif m == '/':
+                        res = a / b
+                    elif m == '**':
+                        res = a**b
+                    print(f"{a} {m} {b} = {res}")
+except:
     print('Введены некорректные данные. Попробуйте ещё раз.')
-if m not in mv:
-    print(f"Я не умею выполнять арифметическое действие '{m}' ")
-elif m == '/' and b == 0:
-    print('Деление на ноль недопустимо. Попробуйте ещё раз.')
-else:
-    if m == '+':
-        res = a + b
-    elif m == '-':
-        res = a - b
-    elif m == '*':
-        res = a * b
-    elif m == '/':
-        res = a / b
-    elif m == '**':
-        res = a**b
-    print(f"{a} {m} {b} = {res}")
