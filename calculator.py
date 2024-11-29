2036f255d5f100895901538a460d414bd97034ab
E-Leno4ka <egrechman@gmail.com>
2024-11-29 19:24:52 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,31 +1,31 @@
+
 mv=('+', '-', '*', '/', '**')
 
+def ress(a, m, b):
+    if m == '+':
+        res = a + b
+    elif m == '-':
+        res = a - b
+    elif m == '*':
+        res = a * b
+    elif m == '/':
+        res = a / b
+    elif m == '**':
+        res = a ** b
+    print(f"{a} {m} {b} = {res}")
+
 try:
-    a=int(input('First num: '))
-    if not isinstance(a,int):
-        print('Введены некорректные данные. Попробуйте ещё раз.')
+    a = int(input('First num: '))
+    m = input('Math operation (+, -, *, /): ')
+    b = int(input('Second num: '))
+    if m not in mv:
+        print(f"Я не умею выполнять арифметическое действие '{m}' ")
+    elif m == '/' and b == 0:
+        print('Деление на ноль недопустимо. Попробуйте ещё раз.')
     else:
-        m=str(input('Math operation (+, -, *, /): '))
-        if m not in mv:
-            print(f"Я не умею выполнять арифметическое действие '{m}' ")
-        else:
-            b=int(input('Second num: '))
-            if not isinstance(b,int):
-                print('Введены некорректные данные. Попробуйте ещё раз.')
-            else:
-                if m == '/' and b == 0:
-                    print('Деление на ноль недопустимо. Попробуйте ещё раз.')
-                else:
-                    if m == '+':
-                        res = a + b
-                    elif m == '-':
-                        res = a - b
-                    elif m == '*':
-                        res = a * b
-                    elif m == '/':
-                        res = a / b
-                    elif m == '**':
-                        res = a**b
-                    print(f"{a} {m} {b} = {res}")
-except:
+        print('А не посчитать ли нам, благородные кроты?')
+        ress(a, m, b)
+except ValueError:
     print('Введены некорректные данные. Попробуйте ещё раз.')
+except NameError:
+     print('Введены некорректные данные. Попробуйте ещё раз.')
