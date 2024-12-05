f2ebb79f4d87c710d0488bbbb2c0aebc5091c6ec
E-Leno4ka <egrechman@gmail.com>
2024-12-05 21:04:46 +0200

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,18 +1,18 @@
 
-def sum_nested_data(arg):
-    if isinstance(arg, int) or isinstance(arg, float):
-        return arg
-    if isinstance(arg,str):
-        return len(arg)
-    if len(arg) == 0:
+def sum_nested_data(nd):
+    if isinstance(nd, int) or isinstance(nd, float):
+        return nd
+    if isinstance(nd,str):
+        return len(nd)
+    if len(nd) == 0:
         return 0
-    if isinstance(arg, list) or isinstance(arg, tuple) or isinstance(arg, set):
-        a = sum_nested_data(list(arg)[0]) + sum_nested_data(list(arg)[1:])
+    if isinstance(nd, list) or isinstance(nd, tuple) or isinstance(nd, set):
+        a = sum_nested_data(list(nd)[0]) + sum_nested_data(list(nd)[1:])
         return a
-    if isinstance(arg, dict):
-        a = sum_nested_data(list(arg.keys())) + sum_nested_data(list(arg.values()))
+    if isinstance(nd, dict):
+        a = sum_nested_data(list(nd.keys())) + sum_nested_data(list(nd.values()))
         return a
 
 
-data_structure=[[1, 2, 3], {'a':4, 'b':5}, (6, {'cube':7, 'drum':8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
+data_structure=[[1, 2, 3.7], {'a':4, 'b':5}, (6, {'cube':7, 'drum':8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 print(sum_nested_data(data_structure))
